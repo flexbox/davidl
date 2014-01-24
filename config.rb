bb376b1fa8df85416d869feb1244d4424b0f8c8c
David Leuliette <dleuliette@siliconsalad.com>
2014-01-24 08:57:15 +0100

    bump middleman-favicon-maker

--- config.rb
+++ config.rb
@@ -41,9 +41,6 @@ page "/browserconfig.xml", :layout => false
 require 'slim'
 require 'builder'
 
-activate :gzip
-activate :livereload
-
 set :js_dir, 'assets/javascripts'
 set :css_dir, 'assets/stylesheets'
 set :images_dir, 'assets/images'
@@ -54,6 +51,8 @@ set :images_dir, 'assets/images'
 
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
+activate :gzip
+activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -79,6 +78,33 @@ configure :build do
   activate :asset_hash
   activate :favicon_maker,
     :favicon_maker_versions => [ :apple_152, :apple_144, :apple_120, :apple_114, :apple_76, :apple_72, :apple_60, :apple_57, :apple, :fav_196, :fav_160, :fav_96, :fav_32, :fav_16, :fav_png, :fav_ico, :mstile_144 ]
+
+  activate :favicon_maker do |f|
+    f.template_dir  = File.join(root, 'source')
+    f.output_dir    = File.join(root, 'build')
+    f.icons = {
+      "favicon_base.png" => [
+        { icon: "apple-touch-icon-152x152-precomposed.png" },
+        { icon: "apple-touch-icon-144x144-precomposed.png" },
+        { icon: "apple-touch-icon-120x120-precomposed.png" },
+        { icon: "apple-touch-icon-114x114-precomposed.png" },
+        { icon: "apple-touch-icon-76x76-precomposed.png" },
+        { icon: "apple-touch-icon-72x72-precomposed.png" },
+        { icon: "apple-touch-icon-60x60-precomposed.png" },
+        { icon: "apple-touch-icon-57x57-precomposed.png" },
+        { icon: "apple-touch-icon-precomposed.png", size: "57x57" },
+        { icon: "apple-touch-icon.png", size: "57x57" },
+        { icon: "favicon-196x196.png" },
+        { icon: "favicon-160x160.png" },
+        { icon: "favicon-96x96.png" },
+        { icon: "favicon-32x32.png" },
+        { icon: "favicon-16x16.png" },
+        { icon: "favicon.png", size: "16x16" },
+        { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },
+        { icon: "mstile-144x144", format: "png" },
+      ]
+    }
+  end
 
   # Enable cache buster
   # activate :cache_buster
