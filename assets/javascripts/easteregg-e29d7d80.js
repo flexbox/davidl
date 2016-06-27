ae1f5a7b1619f384ad0eb7a77aaf936661c822ba
David Leuliette <dleuliette@gmail.com>
2016-06-27 16:18:13 +0200

    Automated commit at 2016-06-27 14:18:13 UTC by middleman-deploy 1.0.0

--- assets/javascripts/easteregg-e29d7d80.js
+++
@@ -1,1 +0,0 @@
-(function(){console.log("---\n \n Salut, ravi de vous rencontrer !\n \n Int\xe9ress\xe9 par la technique ? J'aime pousser du code jours apr\xe8s jours pour aider \xe0 construire\n la cath\xe9drale moderne nomm\xe9e internet.\n \n Vous aussi vous voulez un meilleur avenir pour le Web ?\n Visitez http://davidl.fr/work.html pour travailler avec moi.\n \n ---\n")}).call(this);
