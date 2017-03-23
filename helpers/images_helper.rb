765331bace8bc99a8a6c55d188be916673d9a483
David Leuliette <dleuliette@gmail.com>
2017-03-23 12:20:49 +0100

    Add custom helper for gravatar

--- helpers/images_helper.rb
+++ helpers/images_helper.rb
@@ -1,11 +1,21 @@
 require 'lib/svg'
+require 'digest/md5'
 
-module SvgHelper
+module ImagesHelper
   def svg_tag(filename, options={})
     root      = Middleman::Application.root
     file_path = File.join(root, 'source', config[:images_dir], filename)
     return '(SVG img not found)' unless File.exists?(file_path)
 
     SVG.inline(file_path, options)
+  end
+
+  def gravatar_for(email)
+    hash = Digest::MD5.hexdigest(email.chomp.downcase)
+    "http://www.gravatar.com/avatar/#{hash}?size=250"
+  end
+
+  def fa(name)
+    return "<i class='fa fa-#{name}' aria-hidden='true'></i>"
   end
 end
