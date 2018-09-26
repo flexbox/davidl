b3b9511fb7f2a63a357b4d47ea98467c9a6903b5
David Leuliette <dleuliette@gmail.com>
2018-09-26 14:54:07 +0100

    :fire: bower and add yarn

--- config.rb
+++ config.rb
@@ -6,7 +6,7 @@ Time.zone = 'Paris'
 I18n.config.enforce_available_locales = false
 
 activate :blog do |blog|
-  blog.name   = 'blog'
+  blog.name = 'blog'
 
   blog.permalink = 'blog/:title.html'
   blog.sources   = 'blog/articles/:title.html'
@@ -16,7 +16,7 @@ activate :blog do |blog|
 end
 
 activate :blog do |forge|
-  forge.name   = 'forge'
+  forge.name = 'forge'
 
   forge.permalink = 'forge/:title.html'
   forge.sources   = 'forge/chapitres/:title.html'
@@ -60,7 +60,8 @@ end
 activate :autoprefixer, browsers: ['last 2 versions', 'Explorer >= 9']
 activate :syntax
 activate :sprockets
-sprockets.append_path File.join(root, 'bower_components')
+::Sass.load_paths << File.join(root, "node_modules")
+sprockets.append_path File.join "#{root}", "node_modules/"
 
 ###
 # Page options, layouts, aliases and proxies
@@ -79,23 +80,6 @@ set :css_dir,    'assets/stylesheets'
 set :images_dir, 'assets/images'
 set :js_dir,     'assets/javascripts'
 
-# Add bower's directory to sprockets asset path
-after_configuration do
-  @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
-  sprockets.append_path File.join "#{root}", @bower_config["directory"]
-end
-
-activate :deploy do |deploy|
-  deploy.deploy_method = :git
-  deploy.branch        = 'gh-pages'
-  deploy.build_before  = true # always use --no-clean options
-end
-
-# Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
-
 configure :build do
   activate :favicon_maker do |f|
     f.template_dir  = File.join(root, 'source')
@@ -110,7 +94,7 @@ configure :build do
         { icon: 'favicon-96x96.png' },
         { icon: 'favicon-32x32.png' },
         { icon: 'favicon-16x16.png' },
-        { icon: 'favicon.ico', size: '64x64,32x32,24x24,16x16' },
+        { icon: 'favicon.ico', size: '64x64,32x32,24x24,16x16' }
       ]
     }
   end
